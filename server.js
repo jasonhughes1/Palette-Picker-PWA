d262d1171553328c19669da84800ca1574bda69e
Jason Hughes <jasonhughes088@gmail.com>
2018-01-22 16:25:53 -0700

    Update a few api endpoints in the server

--- server.js
+++ server.js
@@ -10,9 +10,30 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.locals.title = 'Palette Picker';
 
+app.locals.palettes = [
+  {id: 'a1', palette: ['color1', 'color2', 'color3', 'color4', 'color5'] },
+  {id: 'b1', palette: ['color1', 'color2', 'color3', 'color4', 'color5'] }
+]
+
 app.use(express.static(__dirname + '/public'));
 
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
+
+
+app.get('/api/v1/palettes', (request, response) => {
+  const palettes = app.locals.palettes;
+
+  response.status(200).json({ palettes })
+});
+
+app.post('/api/v1/palettes', (request, response) => {
+  const id = Date.now();
+  const { palette } = request.body;
+
+  app.locals.palettes.push(message);
+
+  response.status(201).json({id: message});
+})
