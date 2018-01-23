1ea213a062e4f7043dd0ba4b0521ea540558f3f7
Jason Hughes <jasonhughes088@gmail.com>
2018-01-22 19:41:58 -0700

    Cleanup html and css

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,6 +9,7 @@ const generateRandomColor = () => {
   for (let i = 0; i < 6; i++) {
     color += characters[Math.floor(Math.random() * 16)];
   };
+  console.log(color);
   return color;
 };
 
@@ -16,7 +17,7 @@ const generateRandomColor = () => {
 const updateRandomColors = (i) => {
   for (var i = 0; i < 6; i++) {
 
-  if(!$(`.color${i}`).hasClass('unlocked-image')) {
+  if(!$(`.color${i}`).hasClass('locked')) {
     let color = generateRandomColor()
     $(`.color${i}`).css('background-color', color);
     $(`.hex-code${i}`).text(color)
@@ -24,9 +25,8 @@ const updateRandomColors = (i) => {
  };
 };
 
-
 const toggleLockIcon = () => {
-  
+
 }
 
 
