e0a88d84294e251503e252cfa478ef86f3d49a99
Jason Hughes <jasonhughes088@gmail.com>
2018-01-22 17:54:32 -0700

    Add functionality to my js file and make sure jquery is setup properly

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,28 @@
+$(document).ready(() => {
+  updateRandomColors();
+});
 
+const generateRandomColor = () => {
+  console.log('in generate random color function');
+  const characters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += characters[Math.floor(Math.random() * 16)];
+  };
+  return color;
+};
+
+
+const updateRandomColors = (i) => {
+  for (var i = 0; i < 6; i++) {
+
+  if(!$(`.color${i}`).hasClass('unlocked-image')) {
+    let color = generateRandomColor()
+    $(`.color${i}`).css('background-color', color);
+    $(`.hex-code${i}`).text(color)
+  };
+ };
+};
+
+
+$('.generate-button').on('click', updateRandomColors)
