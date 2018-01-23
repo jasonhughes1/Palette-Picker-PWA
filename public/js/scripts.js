66513d6d8caa8230366aa8c76017fa71b11f3013
Jason Hughes <jasonhughes088@gmail.com>
2018-01-23 15:33:33 -0700

    Update toggleLockIcon function

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,31 +3,33 @@ $(document).ready(() => {
 });
 
 const generateRandomColor = () => {
-  console.log('in generate random color function');
   const characters = '0123456789ABCDEF';
   let color = '#';
   for (let i = 0; i < 6; i++) {
     color += characters[Math.floor(Math.random() * 16)];
   };
-  console.log(color);
   return color;
 };
 
 
 const updateRandomColors = (i) => {
   for (var i = 0; i < 6; i++) {
-
-  if(!$(`.color${i}`).hasClass('locked')) {
-    let color = generateRandomColor()
-    $(`.color${i}`).css('background-color', color)
-    $(`.code${i}`).text(color)
+    if(!$(`.color${i}`).hasClass('locked')) {
+      let color = generateRandomColor()
+      $(`.color${i}`).css('background-color', color)
+      $(`.code${i}`).text(color)
+    };
   };
- };
 };
 
-const toggleLockIcon = () => {
-
+const toggleLockIcon = (event) => {
+  const icon = $(event.target);
+  if (!icon.hasClass('locked')) {
+    icon.closest('.unlocked-image').toggleClass('locked');
+  } else {
+    icon.closest('.unlocked-image').toggleClass('locked');
+  }
 }
 
-
+$('.unlocked-image').on('click', event => toggleLockIcon(event));
 $('.generate-button').on('click', updateRandomColors)
